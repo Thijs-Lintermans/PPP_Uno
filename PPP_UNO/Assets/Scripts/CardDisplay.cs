ffda3e30c5ca9117c1faf6c8656964fc425e983f
ThijsLintermans <thijslintermans2004@gmail.com>
2025-01-31 16:26:30 +0100

    display card 1 en 2

---
+++ PPP_UNO/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    [Header("Colors")]
+    [SerializeField] Color32 red;
+    [SerializeField] Color32 blue;
+    [SerializeField] Color32 yellow;
+    [SerializeField] Color32 green;
+    [SerializeField] Color32 black;
+    [Header("Sprites")]
+    [SerializeField] Sprite reverse;
+    [SerializeField] Sprite skip;
+    [SerializeField] Sprite plusTwo;
+    [SerializeField] Sprite plusFour;
+    [Header("Center Card")]
+    [SerializeField] Image baseCardColor;//this is the main color of the card
+    [SerializeField] Image imageCenter; //this is the color center of the card
+    [SerializeField] Image valueImageCenter; //this is for reverse sprite or skip, ...
+    [SerializeField] TMP_Text valueTextCenter;
+    [SerializeField] GameObject wildImage;
+    [SerializeField] Image topLeftCenter;
+    [SerializeField] Image bottomLeftCenter;
+    [SerializeField] Image topRightCenter;
+    [SerializeField] Image bottomRightCenter;
+    [Header("Top Left Corner")]
+    [SerializeField] Image valueImageTL;
+    [SerializeField] TMP_Text valueTextTL;
+    [SerializeField] GameObject wildImageTL;
+    [SerializeField] Image topLeftTL;
+    [SerializeField] Image bottomLeftTl;
+    [SerializeField] Image topRightTL;
+    [SerializeField] Image bottomRightTL;
+    [Header("Bottom Right Corner")]
+    [SerializeField] Image valueImageBR;
+    [SerializeField] TMP_Text valueTextBR;
+    [SerializeField] GameObject wildImageBR;
+    [SerializeField] Image topLeftBR;
+    [SerializeField] Image bottomLeftBR;
+    [SerializeField] Image topRightBR;
+    [SerializeField] Image bottomRightBR;
+}
